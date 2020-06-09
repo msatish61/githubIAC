1711621a899ef4e1a599d81d0de1dda0ff524d9b
msatish61 <msatish61@gmail.com>
2020-06-09 12:32:22 +0530

    IAC code commit

---
+++ provider.tf
@@ -0,0 +1,6 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = ""
+  features {}
+}
